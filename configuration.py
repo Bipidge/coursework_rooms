9f475ef08309d2906a39c70138be898696c405a8
Ubuntu <azureuser@orchestrator.kx40lgjty3quneeia1j1imzhnc.zx.internal.cloudapp.net>
2025-01-27 02:41:19 +0000

    config

--- configuration.py
+++ configuration.py
@@ -1,10 +1,10 @@
-db_host = "localhost"
+db_host = "roomscoursework.mysql.database.azure.com"
 db_port = 3306
 db_name = "rooms_coursework"
-db_username = "root"
-db_password = "admin"
+db_username = "adeka"
+db_password = "Adeka123!"
 
 api_protocol = "http://"
-api_host = "localhost"
+api_host = "172.167.175.93"
 api_port = 8000
 api_secret_key = "whateverKey"
